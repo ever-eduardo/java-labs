3d071524f8da67f3846772eed9b8ab06d76e80df
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-03-12 19:16:27 -0500

    Create GeometryService.java

---
+++ Geometry1D/src/main/java/dev/everness/geometry1D/application/inputport/GeometryService.java
@@ -0,0 +1,51 @@
+package dev.everness.geometry1D.application.inputport;
+
+import dev.everness.geometry1D.application.usecases.ICRUDGeometry;
+import dev.everness.geometry1D.application.usecases.IOperationsGeometry;
+import dev.everness.geometry1D.domain.Entity;
+import dev.everness.geometry1D.domain.Line;
+import dev.everness.geometry1D.domain.Point;
+
+import java.util.Set;
+
+public class GeometryService implements ICRUDGeometry, IOperationsGeometry {
+    @Override
+    public Set<Entity> getAll() {
+        return null;
+    }
+
+    @Override
+    public Point createPoint(double location) {
+        return new Point(location);
+    }
+
+    @Override
+    public Line createLine(Point a, Point b) {
+        return new Line(a, b);
+    }
+
+    @Override
+    public void deleteEntity(Entity entity) {
+
+    }
+
+    @Override
+    public boolean intersect(Entity left, Entity right) {
+        return false;
+    }
+
+    @Override
+    public boolean isPoint(Entity entity) {
+        return entity instanceof Point;
+    }
+
+    @Override
+    public boolean isLine(Entity entity) {
+        return entity instanceof Line;
+    }
+
+    @Override
+    public boolean isEqual(Entity left, Entity right) {
+        return left.equals(right);
+    }
+}
