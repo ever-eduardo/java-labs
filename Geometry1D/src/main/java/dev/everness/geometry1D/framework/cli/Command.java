71c65ae2c88b2576ef9138b25f1865878332c0d5
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-03-12 22:27:39 -0500

    Create Command.java

---
+++ Geometry1D/src/main/java/dev/everness/geometry1D/framework/cli/Command.java
@@ -0,0 +1,7 @@
+package dev.everness.geometry1D.framework.cli;
+
+public class Command {
+    public static final String EXIT = "exit";
+    public static final String CREATE = "create";
+    public static final String DELETE = "delete";
+}
