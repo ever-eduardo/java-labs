588e5f14a0e823230670662d0a00613aad0733d4
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-03-12 22:31:43 -0500

    Update Command.java

--- Geometry1D/src/main/java/dev/everness/geometry1D/framework/cli/Command.java
+++ Geometry1D/src/main/java/dev/everness/geometry1D/framework/cli/Command.java
@@ -4,4 +4,8 @@ public class Command {
     public static final String EXIT = "exit";
     public static final String CREATE = "create";
     public static final String DELETE = "delete";
+    public static final String WELCOME = """
+            Welcome to Geometry 1D
+            """;
+    public static final String PROMPT = " geometry 1D > ";
 }
