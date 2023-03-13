b8daf8b010d67eee960bbf1b1183c82484368c09
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-03-12 20:42:24 -0500

    First Cli Adapter test

--- Geometry1D/src/main/java/dev/everness/geometry1D/Main.java
+++ Geometry1D/src/main/java/dev/everness/geometry1D/Main.java
@@ -1,7 +1,10 @@
 package dev.everness.geometry1D;
 
+import dev.everness.geometry1D.framework.cli.CommandLineAdapter;
+
 public class Main {
     public static void main(String[] args) {
-        
+        CommandLineAdapter cli = new CommandLineAdapter();
+        cli.run();
     }
 }
