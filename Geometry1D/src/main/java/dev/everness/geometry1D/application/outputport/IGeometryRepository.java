862e124822d0bb954eebc98b8ef3c733a1f40243
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-03-12 19:20:57 -0500

    Update IGeometryRepository.java

--- Geometry1D/src/main/java/dev/everness/geometry1D/application/outputport/IGeometryRepository.java
+++ Geometry1D/src/main/java/dev/everness/geometry1D/application/outputport/IGeometryRepository.java
@@ -1,4 +1,11 @@
 package dev.everness.geometry1D.application.outputport;
 
+import dev.everness.geometry1D.domain.Entity;
+
+import java.util.Set;
+
 public interface IGeometryRepository {
+    Set<Entity> getAll();
+
+    void delete(Entity entity);
 }
