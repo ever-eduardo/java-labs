bcad8e1b5b2c49d9436caa10a84ce17c082e7138
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-03-13 23:04:53 -0500

    Add override methos to CoordenateSystemLocation

--- Geometry1D/src/main/java/dev/everness/geometry1D/domain/value/CoordinateSystemLocation.java
+++ Geometry1D/src/main/java/dev/everness/geometry1D/domain/value/CoordinateSystemLocation.java
@@ -1,14 +1,12 @@
 package dev.everness.geometry1D.domain.value;
 
+import java.util.Objects;
+
 public class CoordinateSystemLocation {
     private double location;
 
     private CoordinateSystemLocation(double location) {
         this.location = location;
-    }
-
-    public double getLocation() {
-        return location;
     }
 
     public static CoordinateSystemLocation createLocation(double location) {
@@ -17,5 +15,22 @@ public class CoordinateSystemLocation {
         if (Double.isNaN(location))
             throw new RuntimeException("CoordinateSystemLocation cannot be NaN");
         return new CoordinateSystemLocation(location);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CoordinateSystemLocation that)) return false;
+        return Double.compare(that.location, location) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location);
+    }
+
+    @Override
+    public String toString() {
+        return "Location(" + location + ')';
     }
 }
